50cbaf2c5763a781cc373f4a531ee9b30224b1a2
jbeltran <jbeltran@ultracom.com.co>
2024-02-13 02:51:50 -0500

    Agrega dependencias faltantes

--- rick_morty.js
+++ rick_morty.js
@@ -39,7 +39,7 @@ async function migrateCharactersAndLocations() {
 					status_character:character.status,
 					character_species:character.species,
 					character_gender:character.gender
-                }
+			}}
 				else if (isPrime(character.id)) { // Incluir a Rick Sanchez con ID 1
                 // Mapear datos del personaje a propiedades de contacto en HubSpot
                 const contactProperties = {
@@ -49,7 +49,7 @@ async function migrateCharactersAndLocations() {
 					status_character:character.status,
 					character_species:character.species,
 					character_gender:character.gender
-                };
+                }};
 				console.log("contactProperties:", contactProperties)
 				// Crear o actualizar el contacto en HubSpot
 				const contactId = await upsertContact(character.id,contactProperties);
